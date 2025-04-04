71c40793925a576e75e97c581591fc4dce41a6d1
Mauricio González Prendas <me@maugp.com>
2025-04-04 07:53:44 -0600

    Add SQL script to create tablespaces and user schema for GE

---
+++ SQL_Scripts/00.sql
@@ -0,0 +1,34 @@
+
+-- Crear los tablespaces GE_Data y GE_Index
+CREATE TABLESPACE GE_Data
+    DATAFILE '/u01/app/oracle/oradata/XE/gedata01.dbf'
+    SIZE 10M
+    REUSE
+    AUTOEXTEND ON
+    NEXT 512k
+    MAXSIZE 200M;
+
+CREATE TABLESPACE GE_Index
+    DATAFILE '/u01/app/oracle/oradata/XE/geindex01.dbf'
+    SIZE 10M
+    REUSE
+    AUTOEXTEND ON
+    NEXT 512k
+    MAXSIZE 200M;
+
+-- 2. Crear el esquema GE
+CREATE USER GE 
+    IDENTIFIED BY ge
+    DEFAULT TABLESPACE GE_Data
+    QUOTA 10M ON GE_Data
+    TEMPORARY TABLESPACE temp
+    QUOTA 5M ON SYSTEM
+    QUOTA 10M ON GE_Index;
+
+GRANT CONNECT to GE;
+GRANT CREATE SESSION to GE;
+GRANT CREATE TABLE to GE;
+
+-- ============================================
+-- Crear las tablas
+-- ============================================
