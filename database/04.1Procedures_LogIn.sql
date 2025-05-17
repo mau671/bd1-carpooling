975f66b99016f425a75188019e455c60285278d6
Dylan Meza <dilanmezamarin@gmail.com>
2025-05-17 17:49:35 -0600

    Update 04.1Procedures_LogIn.sql

--- database/04.1Procedures_LogIn.sql
+++ database/04.1Procedures_LogIn.sql
@@ -23,15 +23,21 @@ CREATE OR REPLACE PROCEDURE validar_usuario (
     p_password  IN VARCHAR2,
     p_es_valido OUT NUMBER
 ) AS
-    v_count INTEGER := 0;
+    v_count INTEGER;
 BEGIN
+    -- Busca en USERNAME o en EMAIL
     SELECT COUNT(*)
     INTO v_count
     FROM PU.PERSONUSER u
     LEFT JOIN PU.EMAIL e ON u.PERSON_ID = e.PERSON_ID
-    WHERE (u.USERNAME = p_input OR e.NAME = p_input)
-      AND u.PASSWORD = p_password;
+    WHERE u.PASSWORD = p_password
+      AND (u.USERNAME = p_input OR e.NAME = p_input);
 
-    p_es_valido := CASE WHEN v_count > 0 THEN 1 ELSE 0 END;
+    IF v_count > 0 THEN
+        p_es_valido := 1;
+    ELSE
+        p_es_valido := 0;
+    END IF;
 END validar_usuario;
 /
+
