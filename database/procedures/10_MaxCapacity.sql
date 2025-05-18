13ff82857add99e5f8d8feb5c20497b4597b41f8
Carmen Hidalgo <hidalgo.paz.carmen@gmail.com>
2025-05-18 00:57:32 -0600

    Se agrega un grant para ADM y se crea el pkg de MaxCapacity

---
+++ database/procedures/10_MaxCapacity.sql
@@ -0,0 +1,34 @@
+-- ============================================================================
+-- PACKAGE: ADM_MAXCAPACITY_PKG
+-- Description: Manages operations related to the capacity of the vehicles in the system
+-- ============================================================================
+CREATE OR REPLACE PACKAGE ADM.ADM_MAXCAPACITY_PKG AS
+    TYPE ref_cursor_type IS REF CURSOR;
+    
+    -- Get all vehicle capacities info
+    FUNCTION get_all_max_capacity
+    RETURN ref_cursor_type;
+
+END ADM_MAXCAPACITY_PKG;
+/
+
+CREATE OR REPLACE PACKAGE BODY ADM.ADM_MAXCAPACITY_PKG AS
+    -- Get all vehicle capacities info
+    FUNCTION get_all_max_capacity
+    RETURN ref_cursor_type
+    AS
+        v_cursor ref_cursor_type;
+    BEGIN
+        OPEN v_cursor FOR
+            SELECT id, capacity_number
+            FROM ADM.MAXCAPACITY
+            ORDER BY capacity_number;
+        RETURN v_cursor;
+    EXCEPTION
+        WHEN OTHERS THEN
+            IF v_cursor%ISOPEN THEN CLOSE v_cursor; END IF;
+            RAISE;
+    END get_all_max_capacity;
+    
+END ADM_MAXCAPACITY_PKG;
+/
